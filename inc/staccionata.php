c0b7f474456fb6b95d676688c714146ac061cb3c
tes <tes@istitutomontani.edu.it>
2022-11-10 10:40:41 +0100

    backup

---
+++ inc/staccionata.php
@@ -0,0 +1,18 @@
+<?php
+
+$string = "PIANTARE IL CAMPO DIETRO LA COLLINA";
+$res = [];
+$key = 3;
+$cont = 0;
+
+//provare trasformandolo in una matrice
+
+for($i=0; $i<$key; $i++){
+    $res[$i] = "";
+    foreach($string as $k => $char){
+        
+    }
+}
+
+
+?>
