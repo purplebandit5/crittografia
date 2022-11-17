d0a5ef1fe382863f0cd90b6b6f4810ffee68f2d8
tes <tes@istitutomontani.edu.it>
2022-11-17 10:33:40 +0100

    backup

---
+++ otp.php
@@ -0,0 +1,35 @@
+<?php
+$testo = "La Passione Che Ci Piace";
+$testo = str_replace(" ", "", $testo); //rimozione degli spazi
+$testo = strtolower($testo); //trasformazione dei caratteri della stringa in minuscoli
+$key = "Al nessiopa hec ic acepi";
+$key = str_replace(" ", "", $key); //rimozione degli spazi
+$key = strtolower($key); //trasformazione dei caratteri della chiave in minuscolo
+$res = "";
+
+
+if (strlen($testo) === strlen($key)) {
+    foreach (str_split($testo) as $index => $char) {
+        $res = $res . chr((ord($char) - 97 + ord($key[$index]) - 97) % 26 + 97);
+    }
+    echo "Criptato: ".$res;
+
+} else {
+    echo "Chiave non valida!";
+}
+
+$testo = "";
+if (strlen($testo) === strlen($key)) {
+    foreach (str_split($res) as $index => $char) {
+        $testo = $testo . chr((ord($char) - 97 - ord($key[$index]) - 97) % 26 + 97);
+    }
+    echo "Decriptato: ".$testo;
+
+} else {
+    echo "Chiave non valida!";
+}
+
+
+
+
+?>
--- staccionata.php
+++ staccionata.php
@@ -1,5 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/mystyle.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.bundle.js"></script>
 
+
+    <title>Crittografia</title>
+</head>
+<?php
 $stringa = "la passione che ci lega";
 $stringa = str_replace(" ", "", $stringa);
 $key = 4;
@@ -45,6 +56,47 @@ foreach ($result as $row) {
     }
 
 }
-
-echo $stringa;
 ?>
+<body>
+    <div class="container-fluid vh-100">
+        <div class="row h-20 align-items-center primary-custom">
+            <!--Header della pagina-->
+            <div class="col-md-12 text-center">
+                <h1>Crittografia</h1>
+            </div>
+        </div>
+        <div class="row overflow-auto pt-1 pb-1 h-80">
+            <div class="col-md-7 border-end">
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle purple" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Scegli il metodo
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="staccionata.php">Cifrario a staccionata</a></li>
+                        <li><a class="dropdown-item" href="#">Cifrario di Cesare</a></li>
+                        <li><a class="dropdown-item" href="#">Cifrario OTP</a></li>
+                        <li><a class="dropdown-item" href="#">Cifrario DES</a></li>
+                    </ul>
+                </div>
+                <br>
+                <h4>Testo input</h4>
+                <form action="index.html" method="post">
+                    <textarea class="form-control" placeholder="Plain text" id="TextAreaDecrypted" rows="4"></textarea> <br>
+                    <h4>Key</h4>
+                    <input class="form-control" type="text" name="Key" placeholder="Enter Key" required><br>
+                    <input type="submit" value="Crypt" class="btn btn-outline purple width">
+                    <input type="submit" value="Decrypt" class="btn btn-outline purple width">
+                    <input type="reset" value="Reset" class="btn btn-outline purple width">
+                </form>
+                <br>
+                <h4>Testo Output</h4>
+                <textarea readonly class="form-control" placeholder="Visualizzazione del testo finale" id="TextAreaCrypted" rows="4"><?php echo $stringa;?></textarea>
+            </div>
+            <div class="col-md-5">
+                <h4 class="text-center">Descrizione</h4>
+                <p class="color"> Il cifrario a staccionata è un tipo di cifrario a trasposizione  che deve il suo nome al modo in cui il testo in chiaro viene cifrato: esso viene trascritto lettera per lettera su righe ideali, diagonalmente verso il basso e poi risalendo una volta arrivati alla riga più bassa e viceversa arrivati alla riga più in alto, disegnando ipotetiche traverse di un'immaginaria staccionata. <br> I cifrari a staccionata non sono molto robusti: il numero di parole chiavi utilizzabili è abbastanza piccolo tanto che un crittanalista le può provare tutte praticamente a mano. </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
